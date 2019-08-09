f2a6167f4fc4e064718e48efe315424db9ee2e0e
471D38UNNUX <52738122+471D38UNNUX@users.noreply.github.com>
2019-08-09 13:07:08 +0700

    Update and rename Metasploit-Framework-Installer-For-Termux.sh to Metasploit-Framework-Installer-For-Termux1.sh

--- Metasploit-Framework-Installer-For-Termux1.sh
+++ Metasploit-Framework-Installer-For-Termux1.sh
@@ -1,7 +1,7 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
 apt update && apt upgrade -y
-apt install unstable-repo -y
+pkg install unstable-repo -y
 apt install metasploit -y
 apt install postgresql -y
 
