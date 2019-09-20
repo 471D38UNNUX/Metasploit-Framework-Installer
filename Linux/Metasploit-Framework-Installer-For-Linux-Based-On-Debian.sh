cc2b30812da4e6980d0abebfe40ad657a6091e24
471D38UNNUX <52738122+471D38UNNUX@users.noreply.github.com>
2019-09-20 22:23:04 +0700

    Update Metasploit Framework Installer

--- Linux/Metasploit-Framework-Installer-For-Linux-Based-On-Debian.sh
+++ Linux/Metasploit-Framework-Installer-For-Linux-Based-On-Debian.sh
@@ -3,7 +3,7 @@
 sudo apt update && sudo apt full-upgrade -y
 sudo apt install wget gnupg gnupg1 gnupg2 -y
 
-sudo echo 'deb http://downloads.metasploit.com/data/releases/metasploit-framework/apt lucid main' > /etc/apt/sources.list.d/metasploit-framework.list
+sudo echo 'deb http://apt.metasploit.com/ lucid main' > /etc/apt/sources.list.d/metasploit-framework.list
 sudo wget -O - http://apt.metasploit.com/metasploit-framework.gpg.key | apt-key add -
 sudo apt update
 sudo apt install metasploit-framework -y
