81d2da9d9c5ef173d6c09660aae1bbdcc9959e66
471D38UNNUX <52738122+471D38UNNUX@users.noreply.github.com>
2019-09-20 22:20:27 +0700

    Update Metasploit Framework Installer

--- Linux/Metasploit-Framework-Installer-For-Linux-Based-On-Red-Hat-Linux.sh
+++ Linux/Metasploit-Framework-Installer-For-Linux-Based-On-Red-Hat-Linux.sh
@@ -3,13 +3,16 @@
 sudo yum update -y
 sudo yum install curl gnupg gnupg1 gnupg2 -y
 
-sudo echo [metasploit] > /etc/yum.repos.d/metasploit-framework.repo
-sudo echo name=Metasploit >> /etc/yum.repos.d/metasploit-framework.repo
-sudo echo baseurl=http://downloads.metasploit.com/data/releases/metasploit-framework/rpm >> /etc/yum.repos.d/metasploit-framework.repo
-sudo echo gpgcheck=1 >> /etc/yum.repos.d/metasploit-framework.repo
-sudo echo gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-Metasploit >> /etc/yum.repos.d/metasploit-framework.repo
-sudo echo enabled=1 >> /etc/yum.repos.d/metasploit-framework.repo
-sudo curl http://apt.metasploit.com/metasploit-framework.gpg.key > /etc/pki/rpm-gpg/RPM-GPG-KEY-Metasploit
+
+echo "Enter your root password."
+su -c'
+echo [metasploit] > /etc/yum.repos.d/metasploit-framework.repo
+echo name=Metasploit >> /etc/yum.repos.d/metasploit-framework.repo
+echo baseurl=http://rpm.metasploit.com/ >> /etc/yum.repos.d/metasploit-framework.repo
+echo gpgcheck=1 >> /etc/yum.repos.d/metasploit-framework.repo
+echo gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-Metasploit >> /etc/yum.repos.d/metasploit-framework.repo
+echo enabled=1 >> /etc/yum.repos.d/metasploit-framework.repo
+curl http://apt.metasploit.com/metasploit-framework.gpg.key > /etc/pki/rpm-gpg/RPM-GPG-KEY-Metasploit'
 
 sudo yum update -y
 sudo yum install metasploit-framework -y
