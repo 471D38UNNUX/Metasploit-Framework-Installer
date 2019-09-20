bc6a4b6546180d1cb44c27e8e405dd47dbd76339
471D38UNNUX <52738122+471D38UNNUX@users.noreply.github.com>
2019-09-20 08:57:06 +0700

    Update Metasploit Framework Installer

--- Linux/Metasploit-Framework-Installer-For-Linux-Based-On-Red-Hat-Linux.sh
+++ Linux/Metasploit-Framework-Installer-For-Linux-Based-On-Red-Hat-Linux.sh
@@ -6,6 +6,7 @@ sudo yum install curl gnupg gnupg1 gnupg2 -y
 sudo echo [metasploit] > /etc/yum.repos.d/metasploit-framework.repo
 sudo echo name=Metasploit >> /etc/yum.repos.d/metasploit-framework.repo
 sudo echo baseurl=http://downloads.metasploit.com/data/releases/metasploit-framework/rpm >> /etc/yum.repos.d/metasploit-framework.repo
+sudo echo gpgcheck=1 >> /etc/yum.repos.d/metasploit-framework.repo
 sudo echo gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-Metasploit >> /etc/yum.repos.d/metasploit-framework.repo
 sudo echo enabled=1 >> /etc/yum.repos.d/metasploit-framework.repo
 sudo curl http://apt.metasploit.com/metasploit-framework.gpg.key > /etc/pki/rpm-gpg/RPM-GPG-KEY-Metasploit
