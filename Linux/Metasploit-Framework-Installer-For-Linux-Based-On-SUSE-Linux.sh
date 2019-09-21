1ff112fd7936b126322fae14cce0c99d6235b8a6
471D38UNNUX <52738122+471D38UNNUX@users.noreply.github.com>
2019-09-21 16:54:04 +0700

    Update Metasploit Framework Installer

--- Linux/Metasploit-Framework-Installer-For-Linux-Based-On-SUSE-Linux.sh
+++ Linux/Metasploit-Framework-Installer-For-Linux-Based-On-SUSE-Linux.sh
@@ -3,14 +3,16 @@
 sudo zypper dup -y
 sudo zypper ref
 
-sudo echo [metasploit] > /etc/zypp/repos.d/metasploit-framework.repo
-sudo echo name=Metasploit >> /etc/zypp/repos.d/metasploit-framework.repo
-sudo echo enabled=1 >> /etc/zypp/repos.d/metasploit-framework.repo
-sudo echo autorefresh=0 >> /etc/zypp/repos.d/metasploit-framework.repo
-sudo echo baseurl=http://downloads.metasploit.com/data/releases/metasploit-framework/rpm >> /etc/zypp/repos.d/metasploit-framework.repo
-sudo echo type=rpm-md >> /etc/zypp/repos.d/metasploit-framework.repo
-sudo echo gpgcheck=1 >> /etc/zypp/repos.d/metasploit-framework.repo
-sudo echo gpgkey=http://apt.metasploit.com/metasploit-framework.gpg.key >> /etc/zypp/repos.d/metasploit-framework.repo
+echo "Enter your root password."
+su -c'
+echo [metasploit] > /etc/zypp/repos.d/metasploit-framework.repo
+echo name=Metasploit >> /etc/zypp/repos.d/metasploit-framework.repo
+echo enabled=1 >> /etc/zypp/repos.d/metasploit-framework.repo
+echo autorefresh=0 >> /etc/zypp/repos.d/metasploit-framework.repo
+echo baseurl=http://downloads.metasploit.com/data/releases/metasploit-framework/rpm >> /etc/zypp/repos.d/metasploit-framework.repo
+echo type=rpm-md >> /etc/zypp/repos.d/metasploit-framework.repo
+echo gpgcheck=1 >> /etc/zypp/repos.d/metasploit-framework.repo
+echo gpgkey=http://apt.metasploit.com/metasploit-framework.gpg.key >> /etc/zypp/repos.d/metasploit-framework.repo'
 
 sudo zypper ref
 sudo zypper install metasploit-framework -y
