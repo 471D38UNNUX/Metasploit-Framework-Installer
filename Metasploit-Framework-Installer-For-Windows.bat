3420a3e09e620188e8740eefca84a27a7f4f16a2
471D38UNNUX <52738122+471D38UNNUX@users.noreply.github.com>
2019-11-24 15:36:31 +0700

    Update Metasploit Framework Installer

--- Metasploit-Framework-Installer-For-Windows.bat
+++ Metasploit-Framework-Installer-For-Windows.bat
@@ -3,5 +3,3 @@
 curl http://windows.metasploit.com/metasploitframework-latest.msi -o C:\Users\%username%\Downloads\metasploitframework-latest.msi
 msiexec /i "C:\Users\%username%\Downloads\metasploitframework-latest.msi" INSTALLDIR="C:\metasploit-framework" /qr /norestart 
 del C:\Users\%username%\Downloads\metasploitframework-latest.msi
-
-msfconsole
