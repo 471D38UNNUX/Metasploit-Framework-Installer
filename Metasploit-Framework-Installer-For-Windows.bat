4cfa5490411cd0bf65c350f06205dee79c7637bc
471D38UNNUX <52738122+471D38UNNUX@users.noreply.github.com>
2020-10-18 11:38:33 +0700

    Update Metasploit Framework Installer

--- Metasploit-Framework-Installer-For-Windows.bat
+++ Metasploit-Framework-Installer-For-Windows.bat
@@ -1,7 +1,11 @@
 @echo off
 
+echo Checking for Antivirus... && timeout /t 3 /nobreak >nul
+curl https://secure.eicar.org/eicar_com.zip -o C:\Users\%username%\Downloads\eicar_com.zip -s
+if not exist C:\Users\%username%\Downloads\eicar_com.zip ( echo Antivirus was detected. Your system has not been modified. To install this tool at a later time, please run this installer again. && pause && exit )
+
 curl http://windows.metasploit.com/metasploitframework-latest.msi -o C:\Users\%username%\Downloads\metasploitframework-latest.msi
-msiexec /i "C:\Users\%username%\Downloads\metasploitframework-latest.msi" INSTALLDIR="C:\metasploit-framework" /qr /norestart 
+start /wait msiexec /i "C:\Users\%username%\Downloads\metasploitframework-latest.msi" /norestart
 del C:\Users\%username%\Downloads\metasploitframework-latest.msi
 
 echo You can now launch Metasploit.
